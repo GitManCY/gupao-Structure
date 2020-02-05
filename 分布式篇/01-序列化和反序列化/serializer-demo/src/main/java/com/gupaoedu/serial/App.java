f950c1b15a01521b6801d79a88eb9f99311ddaf7
GitManCY <2467902872@qq.com>
2020-02-05 16:24:08 +0800

    Initial commit

---
+++ 分布式篇/01-序列化和反序列化/serializer-demo/src/main/java/com/gupaoedu/serial/App.java
@@ -0,0 +1,28 @@
+package com.gupaoedu.serial;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        ISerializer iSerializer=new FastjsonSerializer();
+//        ISerializer iSerializer=new XmlSerializer();
+        User user=new User();
+        user.setAge(18);
+        user.setName("Mic");
+        user.setHobby("菲菲");
+        user.setSex("男");
+
+        byte[] rs=iSerializer.serializer(user);
+
+        System.out.println(new String(rs));
+
+
+        User user1=iSerializer.deSerializer(rs,User.class);
+        System.out.println(user1+"->"+user1.getSex());
+    }
+}
